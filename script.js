83dd61f43de391647bb42ca6e649ee5636094568
Artem Stepanyuk <aivanov757@gmail.com>
2021-12-13 23:12:50 +0300

    second

--- script.js
+++ script.js
@@ -5,22 +5,24 @@ var timer = setInterval(function() {
     
     if (t >= 0) {
     
-        let days = Math.floor(t / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-        let secs = Math.floor((t % (1000 * 60)) / 1000);
+        let secs = Math.floor(t / 1000);
+        let mins = Math.floor(secs / 60);
+        let hours = Math.floor(mins / 60);
+        let days = Math.floor(hours / 24);
+
+        secs %=60;
+        mins %=60;
+        hours %=24;
+        
+       
+
+        document.getElementById("timer-days").innerHTML = days;
     
-        document.getElementById("timer-days").innerHTML = days +
-        "<span class='label'>дней</span>";
+        document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2);
     
-        document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) +
-        "<span class='label'>часов</span>";
+        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2);
     
-        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) +
-        "<span class='label'>минут</span>";
-    
-        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) +
-        "<span class='label'>секунд</span>";
+        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2);
     
     } 
     
